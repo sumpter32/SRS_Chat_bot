2de91d970c73c5e6259cda4c9b0d983eda0426f6
sumpter32 <sumpter32@gmail.com>
2023-01-07 21:31:05 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.post('/', async (req, res) => {
     const prompt = req.body.prompt;
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `have a persona of Mary Smith a Salesperson/Customer Service Representative at srsdesignsllc.com, experienced salesperson and customer service representative strives to stay on topic, keeping the conversation focused on the products and services offered by srsdesignsllc.coms${prompt}`,
+      prompt: `have a persona an Salesperson/Customer Service Representative at srsdesignsllc.com strives to stay on topic and keeping the conversation focused on the products and services offered by srsdesignsllc.coms ${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
