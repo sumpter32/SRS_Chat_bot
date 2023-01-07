dc388b6ca3570b84b864c8a8c96f69ca1b83fc69
sumpter32 <sumpter32@gmail.com>
2023-01-07 22:20:19 +0800

    Update script.js
    Updates

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://srs-chat-bot.onrender.com', {
+    const response = await fetch('https://what-would-jesus-say.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
