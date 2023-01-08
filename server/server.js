0ec491895d8ae64c125ffeea64816800841a871a
sumpter32 <sumpter32@gmail.com>
2023-01-08 09:02:31 +0800

    jesus3

--- server/server.js
+++ server/server.js
@@ -41,46 +41,48 @@ app.post('/', async (req, res) => {
     // Create a request object
     const request = new Request("https://api.openai.com/v1/models/text-davinci-002/finetune", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
-      },
-      body: data,
-    });
+      headers: {        
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    body: data,
+  });
 
-    // Send the request and get the response
-    const response = await fetch(request);
+  // Send the request and get the response
+  const response1 = await fetch(request);
 
-    // Decode the response as a JSON object
-    const result = await response.json();
+  // Decode the response as a JSON object
+  const result = await response1.json();
 
-    // Check the status code of the response
-    if (result.status.code === 200) {
-      // The request was successful
-      console.log(`Language model trained: ${result.data.model}`);
-    } else {
-      // There was an error
-      console.error(`Error: ${result.status.message}`);
-    }
-
-    // Generate a response using the fine-tuned language model
-    const response = await openai.createCompletion({
-      model: "text-davinci-002",
-      prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-    });
-
-    res.status(200).send({
-      bot: response.data.choices[0].text
-    });
-
-  } catch (error) {
-    console.error(error)
-    res.status(500).send(error || 'Something went wrong');
+  // Check the status code of the response
+  if (result.status.code === 200) {
+    // The request was successful
+    console.log(`Language model trained: ${result.data.model}`);
+  } else {
+    // There was an error
+    console.error(`Error: ${result.status.message}`);
   }
+
+  // Generate a response using the fine-tuned language model
+  const prompt = req.body.prompt;
+  const response2 = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: `${prompt}`,
+    temperature: 0, // Higher values means the model will take more risks.
+    max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+    top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+    frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+    presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+  });
+
+  res.status(200).send({
+    bot: response2.data.choices[0].text
+  });
+
+} catch (error) {
+  console.error(error)
+  res.status(500).send(error || 'Something went wrong');
+}
 })
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
