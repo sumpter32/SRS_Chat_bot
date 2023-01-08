5a11aa3a36aefd49ec8e629ce6861cb0b8bf337a
sumpter32 <sumpter32@gmail.com>
2023-01-08 08:22:32 +0800

    jesus

--- client/script.js
+++ client/script.js
@@ -53,68 +53,75 @@ function chatStripe(isAi, value, uniqueId) {
                     <img 
                       src=${isAi ? bot : user} 
                       alt="${isAi ? 'bot' : 'user'}" 
-                    />
-                </div>
-                <div class="message" id=${uniqueId}>${value}</div>
-            </div>
-        </div>
-    `
-    )
-}
-
-const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    const data = new FormData(form)
-
-    // user's chatstripe
-    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
-
-    // to clear the textarea input 
-    form.reset()
-
-    // bot's chatstripe
-    const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
-
-    // to focus scroll to the bottom 
-    chatContainer.scrollTop = chatContainer.scrollHeight;
-
-    // specific message div 
-    const messageDiv = document.getElementById(uniqueId)
-
-    // messageDiv.innerHTML = "..."
-    loader(messageDiv)
-
-    const response = await fetch('https://srs-chat-bot.onrender.com/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt: data.get('prompt')
-        })
-    })
-
-    clearInterval(loadInterval)
-    messageDiv.innerHTML = " "
-
-    if (response.ok) {
-        const data = await response.json();
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
-
-        typeText(messageDiv, parsedData)
-    } else {
-        const err = await response.text()
-
-        messageDiv.innerHTML = "Something went wrong"
-        alert(err)
-    }
-}
-
-form.addEventListener('submit', handleSubmit)
-form.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-        handleSubmit(e)
-    }
-})
+                      />
+                  </div>
+                  <div class="message" id=${uniqueId}>${value}</div>
+              </div>
+          </div>
+      `
+      )
+  }
+  
+  const handleSubmit = async (e) => {
+      e.preventDefault()
+  
+      const data = new FormData(form)
+  
+      // user's chatstripe
+      chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+  
+      // to clear the textarea input 
+      form.reset()
+  
+      // bot's chatstripe
+      const uniqueId = generateUniqueId()
+      chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+  
+      // to focus scroll to the bottom 
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+  
+      // specific message div 
+      const messageDiv = document.getElementById(uniqueId)
+  
+      // messageDiv.innerHTML = "..."
+      loader(messageDiv)
+  
+      const response = await fetch('https://srs-chat-bot.onrender.com/', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              prompt: data.get('prompt'),
+              model: "text-davinci-002", // Use the fine-tuned language model
+              temperature: 0, // Higher values means the model will take more risks.
+              max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+              top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+              frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+              presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+          })
+      })
+  
+      clearInterval(loadInterval)
+      messageDiv.innerHTML = " "
+  
+      if (response.ok) {
+          const data = await response.json();
+          const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+  
+          typeText(messageDiv, parsedData)
+      } else {
+          const err = await response.text()
+  
+          messageDiv.innerHTML = "Something went wrong"
+          alert(err)
+      }
+  }
+  
+  form.addEventListener('submit', handleSubmit)
+  form.addEventListener('keyup', (e) => {
+      if (e.keyCode === 13) {
+          handleSubmit(e)
+      }
+  })
+  
--- server/server.js
+++ server/server.js
@@ -23,15 +23,54 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
   try {
-    const prompt = req.body.prompt;
+    // The texts you want to use as the training dataset
+    const texts = [
+      'Blessed are the poor in spirit, for theirs is the kingdom of heaven.',
+      'Love your enemies and pray for those who persecute you.',
+      'For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.',
+      'I am the way, the truth, and the life. No one comes to the Father except through me.',
+      'Peace I leave with you; my peace I give you. I do not give to you as the world gives. Do not let your hearts be troubled and do not be afraid.',
+      ...
+    ];
 
+    // Encode the texts as a JSON array
+    const data = JSON.stringify({
+      model: "text-davinci-002",
+      prompt: texts,
+    });
+
+    // Create a request object
+    const request = new Request("https://api.openai.com/v1/models/text-davinci-002/finetune", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: data,
+    });
+
+    // Send the request and get the response
+    const response = await fetch(request);
+
+    // Decode the response as a JSON object
+    const result = await response.json();
+
+    // Check the status code of the response
+    if (result.status.code === 200) {
+      // The request was successful
+      console.log(`Language model trained: ${result.data.model}`);
+    } else {
+      // There was an error
+      console.error(`Error: ${result.status.message}`);
+    }
+
+    // Generate a response using the fine-tuned language model
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `persona of jesus${prompt}`,
+      model: "text-davinci-002",
+      prompt: `${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      top_p: 1, // alternative to
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
